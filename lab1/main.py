d035bddd1e77ec9f523409fb0825cfc1c879ad27
kirill <kirill@kirills-MacBook-Air.local>
2023-03-30 14:51:39 +0300

    add function for list

--- lab1/main.py
+++ lab1/main.py
@@ -10,5 +10,14 @@ def mult(x, y):
 def div(x, y):
 	return x / y
 
+def foo(list):
+	result = []
+	for el in list:
+		if el % 2 == 0:
+			result.append(el)
+	return result
+	
+
 print("Hello, world")
 
+print(foo([1, 2, 3, 4]))
