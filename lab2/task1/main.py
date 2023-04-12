70406e4c2954641f6eee77eb294a06b3edc7c2d3
Kirill <mmalihtorovic@gmail.com>
2023-04-12 05:21:41 +0300

    add number of sentences function

--- lab2/task1/main.py
+++ lab2/task1/main.py
@@ -1,4 +1,41 @@
 import os
+import re
+import regexp
+
+
+
+
+def number_of_sentences(text: str):
+    count = len(re.findall(regexp.SENTENCE_PATTERN, text))
+
+    quote_splitting = text.split("\"")
+    for i in range(0, len(quote_splitting)):
+        if i % 2 == 0 and i != 0:
+            count += len(re.findall("^ [A-Z]", quote_splitting[i]))
+
+            if i == len(quote_splitting) - 1 and quote_splitting[i] == "":
+                count += 1
+        elif i % 2 == 1:
+            count -= len(re.findall(regexp.SENTENCE_PATTERN, quote_splitting[i]))
+
+    count -= sum(initials.count(".") for initials in re.findall(regexp.INITIALS, text))
+
+    for abbr in regexp.ONE_WORD_ABBREVIATIONS:
+        count -= len(re.findall(abbr + "(?:,+| [a-z])", text))
+
+    for abbr in regexp.NOT_END_ONE_WORD_ABBREVIATIONS:
+        count -= len(re.findall(abbr, text))
+
+    for abbr in regexp.TWO_WORDS_ABBREVIATIONS:
+        count -= len(re.findall(abbr + "(?:,+| [a-z])", text)) * 2
+        count -= len(re.findall(abbr + " [A-Z]", text))
+        count -= len(re.findall(abbr + "$", text))
+
+    return count
+
+
 
 file = open(os.path.join(os.path.dirname(__file__), "data.txt"), "r")
 text = file.readline()
+
+print(number_of_sentences(text))
