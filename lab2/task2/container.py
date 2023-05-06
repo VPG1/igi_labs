fed49f298f513346ba5ca03de15093328d2475bb
Kirill <mmalihtorovic@gmail.com>
2023-05-06 06:13:51 +0300

    add function for add elements in user container

--- lab2/task2/container.py
+++ lab2/task2/container.py
@@ -11,3 +11,16 @@ class Container:
 
     def __init__(self, user_name):
         self.current_user_name = user_name
+
+    def add(self, add_list):
+        for i in range(0, len(add_list)):
+            try:
+                add_list[i] = str(int(add_list[i]))
+            except:
+                pass
+
+        if self.user_containers.get(self.current_user_name) is None:
+            self.user_containers[self.current_user_name] = set(add_list)
+        else:
+            self.user_containers[self.current_user_name] = self.user_containers[self.current_user_name].union(
+                set(add_list))
