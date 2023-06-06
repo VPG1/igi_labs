edc83fb231a385b981f12340f721c73fa43067d5
Kirill <mmalihtorovic@gmail.com>
2023-06-06 08:20:58 +0300

    create serializer project structure

---
+++ lab3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='malihtorovich-serializer',
+    version='0.0.1',
+    packages=['custom_serializer',
+              'custom_serializer.encoder',
+              'custom_serializer.serializers'],
+    url='',
+    license='',
+    author='vpg1',
+    author_email='',
+    description=''
+)
